482600f5ed80555261adb7ab77e8ba1539b4e1f0
Pyrunix <darrentrieu03@gmail.com>
2022-09-20 16:29:43 -0400

    added Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -2,33 +2,46 @@ public class Polynomial {
 	double[] coefficients;
 	
 	public Polynomial(){
-		coefficients = {0};
+		coefficients = new double[]{0};
 	}
 	
-	public Polynomial(double arr){
-		int len = arr.length;
-		for (int i = 0 ; i < len ; i++){
+	public Polynomial(double[] arr){
+		coefficients = new double[arr.length];
+		for (int i = 0 ; i < arr.length ; i++){
 			coefficients[i] = arr[i];
 		}
 	}
 	
 	public Polynomial add(Polynomial other){
-		int len = other.coefficients.length;
-		double[] arr;
-		for (int i = 0 ; i < len ; i++){
-			arr[i] += other.coefficients[i];
+		int len1 = this.coefficients.length;
+		int len2 = other.coefficients.length;
+		int maxLen = Math.max(len1, len2);
+		double[] arr = new double[maxLen];
+		for (int i = 0 ; i < maxLen; i++){
+			if (i >= len1){
+				arr[i] += other.coefficients[i];
+			} else if (i >= len2){
+				arr[i] += this.coefficients[i];
+			} else {
+			arr[i] += other.coefficients[i] + this.coefficients[i];
+			}
 		}
 		return new Polynomial(arr);
 	}
 	
 	public double evaluate(double val){
-		int len = arr.length;
+		int len = coefficients.length;
 		double sum = 0;
 		for (int i = 0; i < len ; i++){
 			sum += Math.pow(val, i) * coefficients[i]; 
 		}
+		return sum;
 	}
-	public boolean hasRoot(double arr){
-		
+
+	public boolean hasRoot(double val){
+		if (evaluate(val) == 0){
+			return true;
+		}
+		return false;
 	}
 }
